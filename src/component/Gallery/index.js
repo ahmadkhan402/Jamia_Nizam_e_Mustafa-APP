00205134a381a3e9945370f83fad9ea586ebac78
Ahmad Saffiullah <sadapukhtoon777@gmail.com>
2024-07-04 13:20:01 +0500

    gallary screen designed

--- src/component/Gallery/index.js
+++ src/component/Gallery/index.js
@@ -1,10 +1,69 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { ScreenWrapper } from 'react-native-screen-wrapper';
+import FrontView from '../frontView';
+import styles from './styles';
 
 export default function GalleryScreen() {
+    const path = [
+        {
+            id: 1,
+            url: require("./../../assets/gallary_image/1.jpg")
+        },
+        {
+            id: 2,
+            url: require("./../../assets/gallary_image/2.jpg")
+        },
+        {
+            id: 3,
+            url: require("./../../assets/gallary_image/3.jpg")
+        },
+        {
+            id: 4,
+            url: require("./../../assets/gallary_image/4.jpg")
+        },
+        {
+            id: 5,
+            url: require("./../../assets/gallary_image/5.jpg")
+        },
+        {
+            id: 6,
+            url: require("./../../assets/gallary_image/6.jpg")
+        },
+        {
+            id: 7,
+            url: require("./../../assets/gallary_image/7.jpg")
+        },
+        {
+            id: 8,
+            url: require("./../../assets/gallary_image/8.jpg")
+        },
+        {
+            id: 9,
+            url: require("./../../assets/gallary_image/9.jpg")
+        },
+        {
+            id: 10,
+            url: require("./../../assets/gallary_image/10.jpg")
+        }
+    ];
+
     return (
-        <View>
-            <Text>GalleryScreen</Text>
-        </View>
+        <ScreenWrapper scrollType='scroll'>
+            <FrontView text={"گالری"} />
+            <View style={styles.container}>
+                {path.map((item, index) => {
+                    return (
+                        <View key={index} style={styles.imageContainer}>
+                            <Image
+                                source={item.url}
+                                style={styles.image}
+                            />
+                        </View>
+                    )
+                })
+                }
+            </View>
+        </ScreenWrapper>
     );
 }
---
+++ src/component/Gallery/styles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+import { Height, Width } from "../../utils/Dimentions";
+import AppCollors from "../../utils/AppCollors";
+
+const styles = StyleSheet.create({
+    container: {
+        width: Width(100),
+        height: Height(40),
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: 'center',
+        alignItems: 'center',
+
+
+    },
+    imageContainer: {
+
+        margin: Width(1),
+        // backgroundColor: AppCollors.bgLight,
+
+    },
+    image: {
+        width: Width(30),
+        height: Height(15),
+    }
+
+});
+export default styles
